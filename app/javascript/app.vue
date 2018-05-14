7214bb89ee81d9b6ae01220a9cf5ff50af445aff
yamit <yamit.cardenas@gmail.com>
2018-05-13 20:55:38 -0500

    added support for vue.js

---
+++ app/javascript/app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Repartelo 2.0 :D"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
