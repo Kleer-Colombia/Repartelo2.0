d4f45297a108be837b5c2855da07771467a23faf
yamit <yamit.cardenas@gmail.com>
2018-09-10 14:34:35 -0500

    adjust namming error

--- app/models/coaching_session.rb
+++ app/models/coaching_session.rb
@@ -2,7 +2,6 @@ class CoachingSession < ApplicationRecord
 
   validates :date, presence: true
   validates :description, presence: true
-  validates :complementary
   has_many :kleererOnCoachingSessions
   has_many :kleerers, through: :kleererOnCoachingSessions
   belongs_to :balance
--- db/migrate/20180910012317_create_coaching_session.rb
+++ db/migrate/20180910012317_create_coaching_session.rb
@@ -1,4 +1,4 @@
-class CreateCoachingSession < ActiveRecord::Migration[5.1]
+class CreateCoachingSessions < ActiveRecord::Migration[5.1]
   def change
     create_table :coaching_sessions do |t|
       t.belongs_to :balance, index: true
