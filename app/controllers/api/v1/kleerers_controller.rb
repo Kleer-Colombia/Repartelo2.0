5bb39aa9ff7cef0762859b1eb811de812df740de
yamit <yamit.cardenas@gmail.com>
2018-11-20 17:00:36 -0500

    adjust service for kleerers

--- app/controllers/api/v1/kleerers_controller.rb
+++ app/controllers/api/v1/kleerers_controller.rb
@@ -4,11 +4,25 @@ module Api
     class KleerersController < ApiController
 
       def find_all
-        send_response(Kleerer.all)
+        send_response(prepare_info(Kleerer.all))
       end
 
       def find_without_co
-        send_response(Kleerer.where.not(name: 'KleerCo'))
+        send_response(prepare_info(Kleerer.where.not(name: 'KleerCo')))
+      end
+
+      private
+
+      def prepare_info(kleerers)
+        data = []
+        kleerers.each do |kleerer|
+          kjson = {}
+          kjson['id'] =  kleerer.id
+          kjson['name'] =  kleerer.name
+          kjson['option'] =  kleerer.option.name
+          data << kjson
+        end
+        data
       end
     end
   end
