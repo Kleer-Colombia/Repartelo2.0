2938299e6952adc639e71a32e3ae832038c3f89a
yamit <yamit.cardenas@gmail.com>
2021-03-18 14:31:22 -0500

    add taxes al admin y la fehca del impuesto no coincide

--- app/services/taxes/add_manual_tax.rb
+++ app/services/taxes/add_manual_tax.rb
@@ -8,19 +8,15 @@ class AddManualTax
 
   def call
 
-    if(@taxInfo[:date].include?(@taxInfo[:taxYear]))
-      tax = ManualTax.new(amount: @taxInfo[:amount],
-                          tax_master_id: @taxInfo[:taxId],
-                          concept: @taxInfo[:concept],
-                          date: @taxInfo[:date],
-                          payment_date: @taxInfo[:paymentDate])
-      tax.save!
-    else
-      raise StandardError, 'La fecha del impuesto no coincide con el tab del año seleccionado, por favor verificalo'
-    end
+    tax = ManualTax.new(amount: @taxInfo[:amount],
+                        tax_master_id: @taxInfo[:taxId],
+                        concept: @taxInfo[:concept],
+                        date: @taxInfo[:date],
+                        payment_date: @taxInfo[:paymentDate])
+    tax.save!
+
 
   rescue StandardError => error
-    puts error.to_s #TODO improve the logs
     errors.add(:messages, "error saving manual tax: #{error.message}")
     errors.add(:error_code, :not_acceptable)
   end
