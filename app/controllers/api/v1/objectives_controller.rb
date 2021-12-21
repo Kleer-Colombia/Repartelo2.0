ff967d50a8e9f3d1be86f813ec6ba65cc76632d9
SantiagoHV <sherrerav@correo.udistrital.edu.co>
2021-12-21 15:13:22 -0500

    methods for get saldos by kleerer and year added

--- app/controllers/api/v1/objectives_controller.rb
+++ app/controllers/api/v1/objectives_controller.rb
@@ -9,6 +9,7 @@ module Api
       end
 
       def find_kleerco_reports
+        find_kleerers_inputs()
         begin
           kleerCo = Kleerer.find_by(name: "KleerCo")
           kleerCo_id = kleerCo.id
@@ -18,6 +19,49 @@ module Api
           halt_message("can't find kleerCo report: #{error.message}", :internal_server_error)
         end
       end
+
+      def find_kleerers_inputs
+        kleerers = []
+        Kleerer.all.each do |kleerer|
+          if kleerer.option.name.include? "meta"
+            kleerers.push({
+                            name: kleerer.name,
+                            inputs: []
+                          })
+
+          end
+
+          saldos = Saldo.where(kleerer_id: kleerer.id)
+          saldos_by_year = separate_in_years saldos
+
+          puts saldos_by_year
+        end
+        puts kleerers
+      end
+
+      private
+
+      #TODO: year and month saldos as a service
+      def separate_in_years data
+        years = {}
+        data.each do |saldo|
+          years = get_one_year_saldos saldo, years
+        end
+        return years
+      end
+
+      def get_one_year_saldos saldo, years
+        date = saldo.created_at.strftime('%Y')
+        unless years[date]
+          years[date] = []
+        end
+        if saldo.balance
+          years[date].push saldo
+        end
+
+        return years
+      end
+
     end
   end
 end
