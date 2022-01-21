e0b8e6d23a94bc6c67a10a747a3dfe3d2c8966d1
SantiagoHV <sherrerav@correo.udistrital.edu.co>
2022-01-21 17:32:34 -0500

    AllObjectivesButton.vue added

---
+++ app/javascript/components/dynamic_reports/AllObjectivesButton.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="button-container">
+    <el-button type="primary" @click="dialogFormVisible = true">Historico objetivos</el-button>
+
+        <el-dialog :title="'Historico de objetivos'" :visible.sync="dialogFormVisible">
+            <el-table 
+                :id="objectives"
+                :data="showObjectives()"
+                border
+                style="width: 100%">
+                <el-table-column
+                    prop="created_at"
+                    label="Fecha">
+                </el-table-column>
+                <el-table-column
+                    prop="amount"
+                    label="Monto">
+                </el-table-column>
+            </el-table>
+        </el-dialog>
+    </div>
+</template>
+
+
+<script>
+
+import InputMoney from '../base/InputMoney.vue'
+import DynamicReportConnector from "../../model/dynamic_report_connector";
+
+export default {
+    props:{
+        objectives: {
+            type: Array,
+            default: ''
+        },
+        year: {
+            type: Number,
+            default: ''
+        }
+    },
+    components: { InputMoney },
+        data () {
+            return {
+                format: 'expense',
+                dialogFormVisible: false,
+                objectiveInfo: {
+                    amount: 0,
+                }
+            }
+        },
+    methods: {
+        showObjectives(){
+            return this.objectives.find(objective => objective.year === this.year).objectives
+            .map(objective => {
+                return {
+                    created_at: objective.created_at.substr(0,9),
+                    amount: objective.amount
+                }
+            })
+        }
+    }
+}
+</script>
+
+<style>
+.button-container{
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 5px
+}
+</style>
