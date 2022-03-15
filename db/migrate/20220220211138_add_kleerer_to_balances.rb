e9cac6983e7482552243a50b814a9a125618fa47
SantiagoHV <sherrerav@correo.udistrital.edu.co>
2022-03-15 17:25:33 -0500

    nulleable

--- db/migrate/20220220211138_add_kleerer_to_balances.rb
+++ db/migrate/20220220211138_add_kleerer_to_balances.rb
@@ -1,5 +1,5 @@
 class AddKleererToBalances < ActiveRecord::Migration[5.2]
   def change
-    add_column :balances, :kleerer_id, :integer
+    add_column :balances, :kleerer_id, :integer, null: true
   end
 end
