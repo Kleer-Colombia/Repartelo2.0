4bba804c9e0ebbdb17092130a6683b5e5b0923cb
SantiagoHV <sherrerav@correo.udistrital.edu.co>
2022-03-21 14:39:45 -0500

    accumulated saldos added

--- app/controllers/api/v1/saldos_controller.rb
+++ app/controllers/api/v1/saldos_controller.rb
@@ -12,6 +12,7 @@ module Api
           validate_parameters [:kleerer_id], params do
             begin
               response = @actions.find_saldos params[:kleerer_id]
+
               send_response response
             rescue StandardError => error
               halt_message("can't find saldos: #{error.message}", :internal_server_error)
